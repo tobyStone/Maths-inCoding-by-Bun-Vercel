36326c7d7bc4a536d9f8a6755a3e07e8945852ef
Toby Stone <40922682+stickmonster@users.noreply.github.com>
2024-09-16 11:09:04 +0100

    tweak to schema for videoData

--- models/videoModel.js
+++ models/videoModel.js
@@ -4,19 +4,24 @@ const { Schema } = mongoose;
 
 const videoSchema = new Schema({
     video: String,
-    time_stops: {
-        type: [String], // Array of time stops
-        default: [] // Default to an empty array if not provided
-    },
+    time_stop_1: String,
+    time_stop_2: String,
+    time_stop_3: String,
+    time_stop_4: String,
+    time_stop_5: String,
+    time_stop_6: String,
+    time_stop_7: String,
     poster: String,
     imgSrc: String,
     imgAlt: String,
-    question_links: {
-        type: [String], // Array of question links
-        default: [] // Default to an empty array if not provided
-    }
+    link_questions_1: String,
+    link_questions_2: String,
+    link_questions_3: String,
+    link_questions_4: String,
+    link_questions_5: String,
+    link_questions_6: String,
+    link_questions_7: String,
 });
-
 
 const pageSchema = new Schema({
     url_stub: String,
